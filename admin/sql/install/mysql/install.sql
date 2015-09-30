003a59718fde2d006a7ebafd2034725ced53a265
Darryl <Darryl@Corban.Home>
2015-09-30 22:30:58 +0100

    SQL updates

--- admin/sql/install/mysql/install.sql
+++ admin/sql/install/mysql/install.sql
@@ -1,8 +1,7 @@
-CREATE TABLE IF NOT EXISTS `#__ddctap_holidayplanner` (
-  `ddctap_holiday_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `#__ddc_user_absence` (
+  `ddc_user_absence_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `holidaydatestart` DATETIME NOT NULL default '0000-00-00 00:00:00',
-  `holidaydateend` DATETIME NOT NULL default '0000-00-00 00:00:00',
+  `recorddate` DATETIME NOT NULL default '0000-00-00 00:00:00',
   `created_by` int(11) NOT NULL,
   `created` DATETIME NOT NULL default '0000-00-00 00:00:00',
   `modified_by` int(11) NOT NULL,
@@ -11,16 +10,15 @@ CREATE TABLE IF NOT EXISTS `#__ddctap_holidayplanner` (
   `approved_by` int(11) NOT NULL,
   `approved_on` DATETIME NOT NULL default '0000-00-00 00:00:00',
   `status` tinyint(3) NOT NULL,
-  PRIMARY KEY (`ddctap_holiday_id`),
-  KEY `holidaydatestart` (`holidaydatestart`),
-  KEY `holidaydateend` (`holidaydateend`),
+  PRIMARY KEY (`ddc_user_absence_id`),
+  KEY `recorddate` (`recorddate`),
   KEY `user_id` (`user_id`),
   KEY `status` (`status`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 
-CREATE TABLE IF NOT EXISTS `#__ddctap_userext` (
-  `ddctap_userext_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `#__ddc_userext` (
+  `ddc_userext_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `userdatestart` DATETIME NOT NULL default '0000-00-00 00:00:00',
   `userdateend` DATETIME NOT NULL default '0000-00-00 00:00:00',
@@ -28,38 +26,34 @@ CREATE TABLE IF NOT EXISTS `#__ddctap_userext` (
   `created` DATETIME NOT NULL default '0000-00-00 00:00:00',
   `modified_by` int(11) NOT NULL,
   `modified` DATETIME NOT NULL default '0000-00-00 00:00:00',
-  `starttime` TIME NOT NULL default '00:00:00',
-  `finishtime` TIME NOT NULL default '00:00:00',
   `jobtitle` int(11) NOT NULL,
   `directreport` int(11) NOT NULL,
-  `parttime` int(11) NOT NULL,
-  `rbgb` int(11) NOT NULL,
-  `shift` int(11) NOT NULL,
-  `dsp` int(11) NOT NULL,
-  PRIMARY KEY (`ddctap_userext_id`),
+  `contract_type` int(11) NOT NULL,
+  PRIMARY KEY (`ddc_userext_id`),
   KEY `user_id` (`user_id`),
   KEY `directreport` (`directreport`),
   KEY `jobtitle` (`jobtitle`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
   
-CREATE TABLE IF NOT EXISTS `#__ddctap_entitled_holiday` (
-  `ddctap_ent_holiday_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `#__ddc_entitled_leave` (
+  `ddc_ent_leave_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `allowance` int(2) NOT NULL default '25',
+  `catid` int(11) NOT NULL,
   `created` DATETIME NOT NULL default '0000-00-00 00:00:00',
   `modified_by` int(11) NOT NULL,
   `modified` DATETIME NOT NULL default '0000-00-00 00:00:00',
-  PRIMARY KEY (`ddctap_ent_holiday_id`)
+  PRIMARY KEY (`ddc_ent_leave_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
-CREATE TABLE IF NOT EXISTS `#__ddctap_options` (
-  `ddctap_option_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `#__ddc_options` (
+  `ddc_option_id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100),
   `description` text,
   `catid` int(11) NOT NULL,
   `created` DATETIME NOT NULL default '0000-00-00 00:00:00',
   `modified` DATETIME NOT NULL default '0000-00-00 00:00:00',
-  PRIMARY KEY (`ddctap_option_id`)
+  PRIMARY KEY (`ddc_option_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
   
   
